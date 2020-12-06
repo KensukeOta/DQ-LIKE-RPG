a368e9c712c030b6d751618f815e74f3f3a79c1e
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-12-06 19:39:10 +0900

    画像マップの表示

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@
 const FONT = '48px monospace';  //  使用フォント
 
 let gFrame = 0;                 //  内部カウンタ
+let gImgMap;                    //  画像。マップ
+
 //  ブラウザ起動イベント
 window.addEventListener('load', () => {
+    gImgMap = new Image(); gImgMap.src = 'img/map.png'; //  マップ画像読み込み
     setInterval(function () { WmTimer() }, 33);         //  33ms間隔で、WmTimer()を呼び出すよう指示(約30.3fps)
 });
 
@@ -14,6 +17,13 @@ function WmTimer() {
 
     const canvas = document.querySelector('canvas');    //  キャンバス要素を取得
     const ctx = canvas.getContext('2d');                //  2D描画コンテキストを取得
+
+    for (let y = 0; y < 16; y++) {
+        for (let x = 0; x < 16; x++) {
+            ctx.drawImage(gImgMap, x * 32, y * 32);
+        }
+    }
+    
     ctx.font = FONT;                                    //  文字フォントを設定
     ctx.fillText('Hello World ' + gFrame, 0, 64);
 }
